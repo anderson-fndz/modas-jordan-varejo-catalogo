2ce1b10dfeef289722957f5cec905a69bc755425
Anderson <anderson.fndz@gmail.com cnfig --global user.name Your>
2023-11-03 10:34:19 -0300

    criado o modal no menu

--- script.js
+++ script.js
@@ -38,3 +38,15 @@ produto.forEach((e) => {
         showSlider()
     }
 })
+
+const buttonOpen = document.querySelector('.icon-open')
+const modal = document.querySelector('.modal-wrapper')
+const buttonClose = document.querySelector('.icon-close')
+
+buttonOpen.addEventListener("click", function(){
+    modal.style.display = 'block'
+})
+
+buttonClose.addEventListener("click", function(){
+    modal.style.display = 'none'
+})
